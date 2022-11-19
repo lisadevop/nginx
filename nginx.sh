47f7474b8050f07651380b9272f6dcc2895a5ee9
elise <lisymkam@yahoo.com>
2022-11-18 22:29:50 -0500

    nginx

---
+++ nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#author:devops student
+#date 11/18/2022
+#nginx installation
+echo "installation of nginx start
+just wait one min...."
+
+
+yum  update
+yum install nginx -y
+ echo "installation complete!!!"
+
